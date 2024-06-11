ab25f18a91252db9ec064b24c62dd384b7168fea
Jhonathaspq <jhonathaspq@gmail.com>
2024-06-11 18:05:42 -0300

    php

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 function gas() {
-    var n1 = document.getElementById("txtn1");
-    var n2 = document.getElementById("txtn2");
-    var valorAlcool = parseFloat(n1.value);
-    var valorGasolina = parseFloat(n2.value);
-    var resultado = valorAlcool / valorGasolina;
-    if (resultado < 0.7) {
-      alert("É melhor abastecer com Álcool");
-    } else alert("É melhor abastecer com Gasolina");
-  }
+  var n1 = document.getElementById("txtn1");
+  var n2 = document.getElementById("txtn2");
+  var valorAlcool = parseFloat(n1.value);
+  var valorGasolina = parseFloat(n2.value);
+  var resultado = valorAlcool / valorGasolina;
+
+  if (resultado === 0) {
+    alert("[ERRO] preencha os dados corretamente");
+  } else if (resultado < 0.7) {
+    alert("É melhor abastecer com Álcool");
+  } else alert("É melhor abastecer com Gasolina");
+}
