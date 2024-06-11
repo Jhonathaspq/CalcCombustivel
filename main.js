17f6f290944af7b08c1b92d49df22b51d50d6a83
Jhonathaspq <jhonathaspq@gmail.com>
2024-06-11 17:23:06 -0300

    criação

---
+++ main.js
@@ -0,0 +1,10 @@
+function gas() {
+    var n1 = document.getElementById("txtn1");
+    var n2 = document.getElementById("txtn2");
+    var valorAlcool = parseFloat(n1.value);
+    var valorGasolina = parseFloat(n2.value);
+    var resultado = valorAlcool / valorGasolina;
+    if (resultado < 0.7) {
+      alert("É melhor abastecer com Álcool");
+    } else alert("É melhor abastecer com Gasolina");
+  }
